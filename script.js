78ca2bd0b3ebdd06858deb77b0fcc8e08c6d60ca
Thrisha Katkuri <katkurithrisha@gmail.com>
2024-12-20 09:12:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var typed = new Typed(".multiple-text", {
+    strings: [
+      "FrontEnd Development ",
+      "Artificial Intelligence",
+      "Machine Learning",
+      "Data Science",
+      "Prompt engineering"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+      document.querySelectorAll(".Projects-section").forEach(function (section) {
+        if (isElementInViewport(section)) {
+          section.classList.remove("animated");
+        } else {
+          section.classList.add("animated");
+        }
+      });
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+      document.querySelectorAll(".AboutMe").forEach(function (section) {
+        if (isElementInViewport(section)) {
+          section.classList.remove("animated");
+        } else {
+          section.classList.add("animated");
+        }
+      });
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+      document.querySelectorAll(".Skills-section").forEach(function (section) {
+        if (isElementInViewport(section)) {
+          section.classList.remove("animated");
+        } else {
+          section.classList.add("animated");
+        }
+      });
+    });
+  });
+  
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+  
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  function resume() {
+    open("https://drive.google.com/file/d/19neiH32VA7GO8MJN4B4ssSwgoAjEwIcN/view?usp=sharing");
+  }
